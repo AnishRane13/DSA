21e70ccf5afce780dc4d0750cdc2a91efc0db607
AnishRane13 <anishrane178@gmail.com>
2024-01-19 22:47:43 +0530

    Commit 19th Jan 2024

--- 3. Array Problems/Easy Problems/Find_Missing_Number.cpp
+++ 3. Array Problems/Easy Problems/Find_Missing_Number.cpp
@@ -1,17 +1,128 @@
+// Find the missing number in an array
+// Problem Statement: Given an integer N and an array of size N-1 containing
+//  N-1 numbers between 1 to N. Find the number(between 1 to N), 
+//  that is not present in the given array.
 
+// Input Format: N = 5, array[] = {1,2,4,5}
+// Result: 3
+// Explanation: In the given array, number 3 is missing. So, 3 is the answer.
+
+
+
+//Brute Force-----------------------------------
+// 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int missingNumber(int arr[],int N){
+//     for(int i=0; i<=N-1;i++){
+//         if (arr[i] != i+1)
+//         {
+//             return i+1;
+//         }
+//     }
+//     return -1;
+// }
+
+// int main(){
+// int N = 5;
+
+// int arr[] = {1,2,4,5};
+// cout<<missingNumber(arr,N);
+
+//     return 0;
+// }
+
+
+//Better ----------------------------------
+//Hashing
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int missingNumber(int arr[],int N){
+//   int hash[N+1] = {0};
+
+//      // storing the frequencies:
+//   for (int i = 0; i < N-1; i++)
+//   {
+//     hash[arr[i]]++;
+//   }
+
+//   for (int i = 1; i <= N; i++)
+//   {
+//     if (hash[i]==0)
+//     {
+//         return i;
+//     }
+//   }
+//     return -1;
+// }
+
+// int main(){
+// int N = 5;
+
+// int arr[] = {1,2,4,5};
+// cout<<missingNumber(arr,N);
+
+//     return 0;
+// }
+// Time complexity = O(2N);
+
+
+
+//Optimal 1-------------------------
+//Sum of all elements and then by subtracting
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int missingNumber(int arr[],int N){
+//     int sum = (N*(N+1))/2;
+//     int value = 0;
+
+//     for (int i = 0; i < N-1; i++)
+//     {
+//         value += arr[i];
+//     }
+//     return (sum - value);
+// }
+
+// int main(){
+// int N = 5;
+
+// int arr[] = {1,2,4,5};
+// cout<<missingNumber(arr,N);
+
+//     return 0;
+// }
+// Time Complexity = O(N);
+
+
+
+
+
+
+//Optimal 2
+//Xor --- a^a = 0; a^b = 1;
 #include <bits/stdc++.h>
 using namespace std;
 
 int missingNumber(int arr[],int N){
-   int xor1 = 0, xor2 = 0;
+  int hash[N+1] = {0};
 
-    for (int i = 0; i < N - 1; i++) {
-        xor2 = xor2 ^ arr[i]; // XOR of array elements
-        xor1 = xor1 ^ (i + 1); //XOR up to [1...N-1]
+     // storing the frequencies:
+  for (int i = 0; i < N-1; i++)
+  {
+    hash[arr[i]]++;
+  }
+
+  for (int i = 1; i <= N; i++)
+  {
+    if (hash[i]==0)
+    {
+        return i;
     }
-    xor1 = xor1 ^ N; //XOR up to [1...N]
-
-    return (xor1 ^ xor2); // the missing number
+  }
+    return -1;
 }
 
 int main(){
@@ -22,6 +133,3 @@ cout<<missingNumber(arr,N);
 
     return 0;
 }
-
-
-
---
+++ 3. Array Problems/Easy Problems/Intersection_of_two_Sorted_Arrays.cpp
@@ -0,0 +1,11 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+int n = 6;
+int arr[n] = {3,4,5,4,1,2};
+cout<<"Second Largest element is: "<<secondLargest(arr,n);
+
+    return 0;
+}
