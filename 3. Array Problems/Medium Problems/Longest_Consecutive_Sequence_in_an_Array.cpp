3fe0b0d890cec534ea52baa9dbfc2d9b2ba1311b
AnishRane13 <anishrane178@gmail.com>
2024-01-23 19:52:50 +0530

    Commit on 23rd Jan 2024

--- 3. Array Problems/Medium Problems/Longest_Consecutive_Sequence_in_an_Array.cpp
+++ 3. Array Problems/Medium Problems/Longest_Consecutive_Sequence_in_an_Array.cpp
@@ -17,37 +17,126 @@
 
 //Brute Force------------------------------
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// bool linearSearch(vector<int>&a, int num) {
+//     int n = a.size(); //size of array
+//     for (int i = 0; i < n; i++) {
+//         if (a[i] == num)
+//             return true;
+//     }
+//     return false;
+// }
+// int longestSuccessiveElements(vector<int>&a) {
+//     int n = a.size(); //size of array
+//     int longest = 1;
+//     //pick a element and search for its
+//     //consecutive numbers:
+//     for (int i = 0; i < n; i++) {
+//         int x = a[i];
+//         int cnt = 1;
+//         //search for consecutive numbers
+//         //using linear search:
+//         while (linearSearch(a, x + 1) == true) {
+//             x += 1;
+//             cnt += 1;
+//         }
+
+//         longest = max(longest, cnt);
+//     }
+//     return longest;
+// }
+
+// int main()
+// {
+//     vector<int> a = {100, 200, 1, 2, 3, 4};
+//     int ans = longestSuccessiveElements(a);
+//     cout << "The longest consecutive sequence is " << ans << "\n";
+//     return 0;
+// }
+
+
+
+
+
+//Better---------------------------
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int longestSuccessiveElements(vector<int>&a) {
+   
+//    if (a.size()==0) return 0;
+//     sort(a.begin(),a.end());
+//     int n = a.size();
+//     int lastSmaller = INT_MIN;
+//     int count = 0;
+//     int longest = 1;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (a[i] - 1 == lastSmaller)
+//         {
+//             count += 1;
+//             lastSmaller = a[i];
+//         }else if (lastSmaller != a[i])
+//         {
+//             count = 1;
+//             lastSmaller = a[i];
+//         }
+//         longest = max(longest, count);
+//     }
+//     return longest;
+// }
+
+// int main()
+// {
+//     vector<int> a = {100, 200, 1, 2, 3, 4};
+//     int ans = longestSuccessiveElements(a);
+//     cout << "The longest consecutive sequence is " << ans << "\n";
+//     return 0;
+// }
+
+
+
+
+
+
+//Optimal Approach --------------------------------
+// Using unordered map
+
 
 
 #include <bits/stdc++.h>
 using namespace std;
 
-bool linearSearch(vector<int>&a, int num) {
-    int n = a.size(); //size of array
-    for (int i = 0; i < n; i++) {
-        if (a[i] == num)
-            return true;
-    }
-    return false;
-}
 int longestSuccessiveElements(vector<int>&a) {
-    int n = a.size(); //size of array
-    int longest = 1;
-    //pick a element and search for its
-    //consecutive numbers:
-    for (int i = 0; i < n; i++) {
-        int x = a[i];
-        int cnt = 1;
-        //search for consecutive numbers
-        //using linear search:
-        while (linearSearch(a, x + 1) == true) {
-            x += 1;
-            cnt += 1;
-        }
+    int n = a.size();
+    if (n == 0) return 0;
 
-        longest = max(longest, cnt);
+    int longest = 1;
+    unordered_set<int> st;
+    //put all the array elements into set:
+    for (int i = 0; i < n; i++) {
+        st.insert(a[i]);
+    }
+
+    //Find the longest sequence:
+    for (auto it : st) {
+        //if 'it' is a starting number:
+        if (st.find(it - 1) == st.end()) {
+            //find consecutive numbers:
+            int cnt = 1;
+            int x = it;
+            while (st.find(x + 1) != st.end()) {
+                x = x + 1;
+                cnt = cnt + 1;
+            }
+            longest = max(longest, cnt);
+        }
     }
     return longest;
+
 }
 
 int main()
---
+++ 3. Array Problems/Medium Problems/Subarray_with_sum_k.cpp
@@ -0,0 +1,52 @@
+// Count Subarray sum Equals K
+// Problem Statement: Given an array of integers and an integer k, return the total number of subarrays whose sum equals k.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+
+
+// Example 1:
+// Input Format: N = 4, array[] = {3, 1, 2, 4}, k = 6
+// Result: 2
+// Explanation: The subarrays that sum up to 6 are [3, 1, 2] and [2, 4].
+
+// Example 2:
+// Input Format: N = 3, array[] = {1,2,3}, k = 3
+// Result: 2
+// Explanation: The subarrays that sum up to 3 are [1, 2], and [3].
+
+
+
+
+//Brute Force ----------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    int cnt = 0; // Number of subarrays:
+    int sum =0;
+    for (int i = 0; i < n; i++)
+    {
+        sum =0;
+        for (int j = i; j < n; j++)
+        {
+            sum += arr[j];
+            if (sum == k)
+            {
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+
+int main()
+{
+    vector<int>arr = {3, 1, 2};
+    int k = 3;
+    int cnt = findAllSubarraysWithGivenSum(arr, k);
+    cout << "The number of subarrays is: " << cnt << "\n";
+    return 0;
+}
+
